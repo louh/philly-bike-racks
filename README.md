781d21c31ac1ae6d8836d6ea098191b18d322cc7
Lou Huang <lou@streetmix.net>
2025-06-16 21:59:48 -0400

    feat: geocode everything

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ```sh
 node index.js ./data/[SOURCE_FILENAME].csv > ./output/[OUTPUT_FILENAME].geojson
 ```
+
+for example:
+
+```sh
+node index.js ./data/PBA\ Bike\ Rack\ Tracker\ -\ ALL\ YEARS.csv > ./output/output-all.geojson
+```
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import fs from "node:fs/promises";
 import "dotenv/config";
 import { parse } from "csv-parse/sync";
 import Pelias from "pelias-js";
+import pMap from 'p-map';
+import pThrottle from "p-throttle";
 
 // We need a data file, look for a file path to be passed in
 // We can use `minimist` if this needs to be more complex, but
@@ -39,49 +41,60 @@ const records = parse(content, {
   skip_empty_lines: true,
 });
 
-// Geocode each address
-const promises = [];
-
-for (let i = 0; i < records.length; i++) {
-  const record = records[i];
+// Create search function
+function doSearch (record) {
   const address = record[COLUMN_NAME];
-  if (address === "") {
-    continue;
-  } else {
-    promises.push(
-      new Promise((resolve, reject) => {
-        pelias.search
-          .setSearchTerm(address)
-          .execute()
-          .then((response) => {
-            resolve({
-              record,
-              response,
-            });
-          })
-          .catch((error) => {
-            console.log(`Error geocoding '${address}': ${error}`);
-            reject({
-              record,
-              error,
-            });
-          });
+  return new Promise((resolve, reject) => {
+    // Skip if no address in record
+    if (address === "") {
+      resolve(null)
+    }
+
+    pelias.search
+      .setSearchTerm(address)
+      .execute()
+      .then((response) => {
+        resolve({
+          record,
+          response,
+        });
       })
-    );
-  }
+      .catch((error) => {
+        console.log(`Error geocoding '${address}': ${error}`);
+        reject({
+          record,
+          error,
+        });
+      });
+  })
 }
 
-const results = await Promise.all(promises);
+// Not sure what the rate limit is but this is conservative.
+// We can probably bump this up until we hit 429s
+const throttle = pThrottle({
+  limit: 10,
+  interval: 1000
+});
+
+const throttled = throttle(doSearch);
+
+// I don't exactly know how many of these we can run at a time
+// This is where we geocode each address
+const results = await pMap(records, throttled, {
+  concurrency: 5
+});
 
 // Output a GeoJSON with the result of the first search result of each
-// geocoding query
-const features = results.map((result) => {
+// geocoding query. Filter out results that are null (no address in source
+// record)
+const features = results.filter(r => r !== null).map((result) => {
   const { record, response } = result;
   const feature = response.features[0];
 
   return {
     type: 'Feature',
-    geometry: feature.geometry,
+    // It's possible to get back an empty feature? So we put null here
+    geometry: feature?.geometry ?? null,
     properties: {
       ...record
     }
